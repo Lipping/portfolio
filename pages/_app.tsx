66edb6b8ede125b8e600207cce90558138f3a028
Lipping <felipelagesstefenoni@gmail.com>
2022-06-27 18:47:48 -0300

    hotjar extension

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import { globalStyles } from '../shared/styles'
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
-const App = ({ Component, pageProps }) => (
-  <>
-    {globalStyles}
-    <Component {...pageProps} />
-  </>
-)
+
+
+function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    hotjar.initialize(3040690, 6)
+  }, [])
+
+  return (
+    <>
+      {globalStyles}
+      <Component {...pageProps} />
+    </>
+  )
+}
 
 export default App
