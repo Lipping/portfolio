58e60a6ad263e62db26d1f333ef94f8cf3ae9239
Lipping <felipelagesstefenoni@gmail.com>
2022-06-28 10:34:13 -0300

    google analytics extension

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { globalStyles } from '../shared/styles'
 import { hotjar } from 'react-hotjar'
 import { useEffect } from 'react'
+import { Router, useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 
 
@@ -9,6 +11,16 @@ function App({ Component, pageProps }) {
   useEffect(() => {
     hotjar.initialize(3040690, 6)
   }, [])
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [Router.events])
 
   return (
     <>
